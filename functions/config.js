c27e7b6e2e49d8334c1163fca94872cff46249e8
diederik <diederikjhattingh@gmail.com>
2026-01-07 21:24:49 +0200

    Fix lint issues

--- functions/config.js
+++ functions/config.js
@@ -2,6 +2,6 @@
 // Change this to your preferred Firebase region
 // Must match the region in public/config.js and firebase.json
 
-const REGION = 'africa-south1';
+const REGION = "africa-south1";
 
-module.exports = { REGION };
+module.exports = {REGION};
